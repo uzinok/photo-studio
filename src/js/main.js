6154e48c7a2f4cf8ffec315b2919b7e26ada1829
uzinok <uzinok@yandex.ru>
2020-08-31 17:20:54 +0400

    работа с бургером - 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
-window.onload = function () {};
+window.onload = function () {
+    let headerButtonJs = document.querySelector('.header__button--js');
+    let nav = document.querySelector('.header__nav nav');
+    
+    headerButtonJs.addEventListener('click', function() {
+        nav.classList.toggle('nav-visible');
+        this.classList.toggle('open');
+    })
+};
