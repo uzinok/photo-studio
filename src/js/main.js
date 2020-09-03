7071794d87b02a5e67d3ce66c8074e5e92be04a3
uzinok <uzinok@yandex.ru>
2020-09-03 19:48:37 +0400

    03.09.2020

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,35 @@
 window.onload = function () {
     let headerButtonJs = document.querySelector('.header__button--js');
     let nav = document.querySelector('.header__nav nav');
+    let links = document.querySelectorAll('.header__nav-link');
     
     headerButtonJs.addEventListener('click', function() {
-        nav.classList.toggle('nav-visible');
+        let countLink = 0;
+        if (nav.classList.contains('hidden-max-xl')) {
+            nav.classList.remove('hidden-max-xl');
+
+            setTimeout(function() {
+                nav.classList.add('nav-visible');
+            }, 10)
+
+            document.addEventListener('keydown', function(e) {
+
+                if (e.keyCode == 9) {
+                    e.preventDefault
+                    links[0].focus();
+                    console.log(links);
+                    countLink++
+                }
+                
+            }, false)
+        } else {
+            nav.classList.remove('nav-visible');
+            setTimeout(function() {
+                nav.classList.add('hidden-max-xl');
+            }, 500)
+            
+        }
         this.classList.toggle('open');
     })
+
 };
