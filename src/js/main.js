a7d09e72ec45c71bd3b329456ba1bc494a8a0b16
uzinok <uzinok@yandex.ru>
2020-09-05 14:30:03 +0400

    видео 05.09.2020

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,31 @@
 window.onload = function () {
     let headerButtonJs = document.querySelector('.header__button--js');
     let nav = document.querySelector('.header__nav nav');
-    let links = document.querySelectorAll('.header__nav-link');
     
     headerButtonJs.addEventListener('click', function() {
-        let countLink = 0;
-        if (nav.classList.contains('hidden-max-xl')) {
+        
+        if(nav.classList.contains('nav-visible')) {
+
+            nav.classList.remove('nav-visible');
+
+            setTimeout(function(){
+                nav.classList.add('hidden-max-xl');
+                headerButtonJs.classList.remove('open');
+            },500);
+
+
+        } else {
+
             nav.classList.remove('hidden-max-xl');
 
-            setTimeout(function() {
+            setTimeout(function(){
                 nav.classList.add('nav-visible');
-            }, 10)
+                headerButtonJs.classList.add('open');
+            },10);
 
-            document.addEventListener('keydown', function(e) {
 
-                if (e.keyCode == 9) {
-                    e.preventDefault
-                    links[0].focus();
-                    console.log(links);
-                    countLink++
-                }
-                
-            }, false)
-        } else {
-            nav.classList.remove('nav-visible');
-            setTimeout(function() {
-                nav.classList.add('hidden-max-xl');
-            }, 500)
-            
         }
-        this.classList.toggle('open');
+        
+        
     })
-
 };
