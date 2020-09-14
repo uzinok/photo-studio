22e8a5d3b491daa254d8d694ef5dab0c789f8999
uzinok <uzinok@yandex.ru>
2020-09-14 16:48:19 +0400

    лень матушка

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,37 @@
 window.onload = function () {
-    let headerButtonJs = document.querySelector('.header__button--js');
-    let nav = document.querySelector('.header__nav nav');
-    
-    headerButtonJs.addEventListener('click', function() {
-        
-        if(nav.classList.contains('nav-visible')) {
 
+    // no-JS
+    var noJS = document.querySelectorAll('.no-JS');
+    noJS.forEach(function (element) {
+        element.classList.remove('no-JS');
+    });
+
+    // nav
+    var headerButtonJs = document.querySelector('.header__button--js');
+    var nav = document.querySelector('.header__nav nav');
+
+    headerButtonJs.addEventListener('click', function () {
+        if (nav.classList.contains('nav-visible')) {
             nav.classList.remove('nav-visible');
-
-            setTimeout(function(){
+            setTimeout(function () {
                 nav.classList.add('hidden-max-xl');
                 headerButtonJs.classList.remove('open');
-            },500);
-
-
+            }, 500);
         } else {
-
             nav.classList.remove('hidden-max-xl');
 
-            setTimeout(function(){
+            setTimeout(function () {
                 nav.classList.add('nav-visible');
                 headerButtonJs.classList.add('open');
-            },10);
-
-
+            }, 10);
         }
-        
-        
-    })
+    });
+
+    // slider
+
+    slider(document.querySelector('.slider'));
+
+    function slider(slider) {
+        console.log(slider);
+    }
 };
