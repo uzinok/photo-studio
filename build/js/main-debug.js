31289f9fd8c3d5dfdb67e172d42fb67901b30665
uzinok <uzinok@yandex.ru>
2020-09-17 19:35:18 +0400

    17/09/2020

--- build/js/main-debug.js
+++ build/js/main-debug.js
@@ -2,9 +2,9 @@ window.onload = function () {
 
     // no-JS
     var noJS = document.querySelectorAll('.no-JS');
-    noJS.forEach(function (element) {
-        element.classList.remove('no-JS');
-    });
+    for (var i = 0; i < noJS.length; i++) {
+        noJS[i].classList.remove('no-JS');
+    }
 
     // nav
     var headerButtonJs = document.querySelector('.header__button--js');
@@ -32,6 +32,141 @@ window.onload = function () {
     slider(document.querySelector('.slider'));
 
     function slider(slider) {
-        console.log(slider);
+
+        var sliderListWrap = slider.querySelector('.slider__list-wrap');
+        var sliderList = slider.querySelector('.slider__list');
+        var sliderItem = slider.querySelectorAll('.slider__item');
+        var pagerButton = slider.querySelectorAll('.slider__pager-button');
+        var count = sliderItem.length;
+        createSlides(sliderItem, sliderList);
+        var arrWirthSlide = [268, 450, 585];
+        var arrBreakpointWidth = [768, 1024, 1450];
+        var countActive = 1;
+        var transitionDelay = 200;
+        sliderList.style.transition = 'transform ' + transitionDelay + 'ms ease-out';
+
+        initSlider(sliderList, arrWirthSlide, arrBreakpointWidth, count, sliderListWrap);
+
+        window.addEventListener("resize", function () {
+            windowResize();
+        });
+
+        var check;
+
+        function windowResize() {
+            clearTimeout(check);
+            check = setTimeout(function () {
+                initSlider(sliderList, arrWirthSlide, arrBreakpointWidth, count, sliderListWrap);
+
+            }, 100);
+        }
+
+        for (var i = 0; i < pagerButton.length; i++) {
+            pagerButton[i].addEventListener('click', function () {
+                countActive = +this.getAttribute('data-count');
+                countActive = drawSlider(countActive, pagerButton, sliderItem, slider);
+            });
+        }
+
+        slider.querySelector('.slider__prev').addEventListener('click', function () {
+            countActive = countActive - 1;
+            countActive = drawSlider(countActive, pagerButton, sliderItem, slider);
+        });
+
+        slider.querySelector('.slider__next').addEventListener('click', function () {
+            countActive = countActive + 1;
+            countActive = drawSlider(countActive, pagerButton, sliderItem, slider);
+        });
+
+        countActive = drawSlider(countActive, pagerButton, sliderItem, slider);
+
+    }
+
+    function initSlider(sliderList, arrWirthSlide, arrBreakpointWidth, count, sliderListWrap) {
+        var wSlide = 0;
+
+        if (window.innerWidth < arrBreakpointWidth[0]) {
+            wSlide = arrWirthSlide[0];
+        } else if (window.innerWidth < arrBreakpointWidth[1]) {
+            wSlide = arrWirthSlide[1];
+        } else {
+            wSlide = arrWirthSlide[2];
+        }
+
+        setSliderWidth(sliderList, wSlide, count, sliderListWrap);
+    }
+
+    function setSliderWidth(sliderList, wSlide, count, sliderListWrap) {
+        sliderList.style.width = (count + 4) * wSlide + 'px';
+
+        if (wSlide == 268) {
+            sliderListWrap.style.width = wSlide + 'px';
+        } else {
+            sliderListWrap.style.width = 100 + '%';
+        }
+    }
+
+    function createSlides(sliderItem, sliderList) {
+
+        var liFirst = document.createElement('li');
+        liFirst.innerHTML = sliderItem[sliderItem.length - 1].innerHTML;
+        liFirst.classList.add('slider__item');
+        liFirst.setAttribute('data-count', 0);
+        sliderList.insertBefore(liFirst, sliderItem[0]);
+
+        var liFirst1 = document.createElement('li');
+        liFirst1.innerHTML = sliderItem[sliderItem.length - 2].innerHTML;
+        liFirst1.classList.add('slider__item');
+        liFirst1.setAttribute('data-count', -1);
+        sliderList.insertBefore(liFirst1, liFirst);
+
+        var liLast = document.createElement('li');
+        liLast.innerHTML = sliderItem[0].innerHTML;
+        liLast.classList.add('slider__item');
+        liLast.setAttribute('data-count', sliderItem.length + 1);
+        sliderList.appendChild(liLast);
+
+        var liLast1 = document.createElement('li');
+        liLast1.innerHTML = sliderItem[1].innerHTML;
+        liLast1.classList.add('slider__item');
+        liLast1.setAttribute('data-count', sliderItem.length + 2);
+        sliderList.appendChild(liLast1);
+
+    }
+
+    function drawSlider(countActive, pagerButton, sliderItem, slider) {
+        if (slider.querySelector('.active')) {
+            slider.querySelector('li.active').classList.remove('active');
+            slider.querySelector('button.active').classList.remove('active');
+            slider.querySelector('[tabindex="0"]').setAttribute('tabindex', "-1");
+        }
+
+        if (countActive == 0) {
+            countActive = sliderItem.length;
+            draw();
+
+        } else if (countActive == sliderItem.length + 1) {
+            countActive = 1;
+            draw();
+        } else {
+            draw();
+        }
+
+        function draw() {
+
+            console.log('data-count="' + countActive + '"');
+            if (slider.querySelector('[style="transform: translate(-10px, 0px);"]')) {
+                slider.querySelector('[style="transform: translate(-10px, 0px);"]').style.transform = '';
+                slider.querySelector('[style="transform: translate(10px, 0px);"]').style.transform = '';
+            }
+            slider.querySelector('[data-count="' + (countActive - 1) + '"]').style.transform = 'translate(-10px, 0px)';
+            slider.querySelector('[data-count="' + (countActive + 1) + '"]').style.transform = 'translate(10px, 0px)';
+            sliderItem[countActive - 1].style.transform = 'translate(0, -20px)';
+            sliderItem[countActive - 1].classList.add('active');
+            pagerButton[countActive - 1].classList.add('active');
+            sliderItem[countActive - 1].querySelector('a').setAttribute('tabindex', "0");
+        }
+
+        return countActive;
     }
 };
